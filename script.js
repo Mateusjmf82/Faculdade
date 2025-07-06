015120db9c49b03df505417a22ee1ae47c15babc
Mateusjmf82 <93928895+Mateusjmf82@users.noreply.github.com>
2025-07-06 17:11:29 -0300

    Projeto Finalizado

---
+++ script.js
@@ -0,0 +1,3 @@
+function mostrarMensagem() {
+  document.getElementById("mensagem").textContent = "✨ Continue pedalando! Cada giro é um passo em direção a novos horizontes e à sua melhor versão. ✨";
+}
